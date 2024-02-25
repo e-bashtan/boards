ac14175529d733572c9fb7856216fb930f06638d
Evgeniy <e.bashtan@qiwi.com>
2024-02-26 00:18:03 +0300

    Board list

---
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -0,0 +1,62 @@
+"use client"
+
+import Image from "next/image";
+import Link from "next/link";
+import { Overlay } from "./overlay";
+import { formatDistanceToNow } from "date-fns";
+import { useAuth } from "@clerk/nextjs";
+import { Footer } from "./footer";
+
+interface BoardCardProps {
+    id: string;
+    title: string;
+    imageUrl: string;
+    authorId: string;
+    authorName: string;
+    createAt: number;
+    orgId: string;
+    isFavorite: bollean;
+}
+
+export const BoardCard = ({
+    id,
+    title,
+    imageUrl,
+    authorId,
+    authorName,
+    createAt,
+    orgId,
+    isFavorite,
+}: BoardCardProps) => {
+    const { userId } = useAuth()
+
+    const authorLabel = userId === authorId ? "You" : authorName
+    const createdAtLabel = formatDistanceToNow(createAt, {
+        addSuffix: true,
+    })
+
+
+    return (
+        <Link href={`/board/${id}`}>
+            <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
+                <div className="relative flex-1 bg-amber-50">
+                    <Image
+                        src={imageUrl}
+                        alt={title}
+                        fill
+                        className="object-fit"
+                    />
+                    <Overlay />
+                </div>
+                <Footer
+                    isFavorite={isFavorite}
+                    title={title}
+                    authorLabel={authorLabel}
+                    createdAtLabel={createdAtLabel}
+                    onClick={() => { }}
+                    disabled={false}
+                />
+            </div>
+        </Link>
+    )
+}
